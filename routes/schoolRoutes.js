ea4c1731988d46895e7993e0cc920065d32783c7
Chief <kameyaw14@gmail.com>
2025-08-06 15:12:18 +0000

    admin dashboard

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,6 +1,6 @@
 // routes/schoolRoutes.js
 import express from 'express';
-import { register, login, addStudent } from '../controller/schoolController.js';
+import { register, login, addStudent, getAdminDashboard } from '../controller/schoolController.js';
 import arcjetMiddleware from '../middleware/arcjet.js';
 import { authenticateSchool } from '../middleware/auth.js';
 
@@ -9,5 +9,6 @@ const schoolRouter = express.Router();
 schoolRouter.post('/register', arcjetMiddleware, register);
 schoolRouter.post('/login', arcjetMiddleware, login);
 schoolRouter.post('/students/add', arcjetMiddleware, authenticateSchool, addStudent);
+schoolRouter.get('/dashboard', arcjetMiddleware, authenticateSchool, getAdminDashboard);
 
 export default schoolRouter;
